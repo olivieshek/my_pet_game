e52d36f5305a3a00c67dd9ce2e37474e49f9e63e
oli <72987619+olivieshek@users.noreply.github.com>
2022-02-16 19:44:52 +0300

    Файл питомца

--- pet.py
+++ pet.py
@@ -13,24 +13,35 @@ class Pet():
         mood_change=1
         ):
 
-        self.portrait = portrait,
-        self.name = name,
-        self.fullness = fullness,
-        self.activity = activity,
-        self.cheerfulness = cheerfulness,
-        self.mood = mood,
-        self.fullness_change = fullness_change,
-        self.activity_change = activity_change,
-        self.cheerfulness_change = cheerfulness_change,
+        self.portrait = portrait
+        self.name = name
+        self.fullness = fullness
+        self.activity = activity
+        self.cheerfulness = cheerfulness
+        self.mood = mood
+        self.fullness_change = fullness_change
+        self.activity_change = activity_change
+        self.cheerfulness_change = cheerfulness_change
         self.mood_change = mood_change
 
+
     def decrease_stats(self):
-        self.fullness -= fullness_change
-        self.activity -= activity_change
-        self.cheerfulness -= cheerfulness_change
-        self.mood -= mood_change
+        self.fullness -= self.fullness_change
+        self.activity -= self.activity_change
+        self.cheerfulness -= self.cheerfulness_change
+        self.mood -= self.mood_change
 
     def feed(self, calories):
         self.fullness += calories
         print(f'{self.name} съел {calories} калорий,'
-        ' и теперь у него {self.fullness} сытости.')
+        f' и теперь у него {self.fullness} очков сытости.')
+
+    def play(self, time):
+        self.activity += time
+        print(f'Вы поиграли с питомцем и теперь'
+        f' у него {self.activity} очков активности.')
+
+    def sleep(self, time):
+        self.cheerfulness += time
+        print(f'Питомец поспал и теперь у него'
+        f' {self.cheerfulness} очков бодрости.')
